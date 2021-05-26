c0a0338b41c70bf0507b13a938706e8e1ba35756
Chris Ahl <cahl@redhat.com>
2021-05-26 15:44:51 -0400

    cleanup unused stuff
    Signed-off-by: Chris Ahl <cahl@redhat.com>

--- pkg/cmd/scale/cluster/exec.go
+++ pkg/cmd/scale/cluster/exec.go
@@ -35,17 +35,17 @@ func (o *Options) validate() (err error) {
 	}
 
 	if o.clusterName == "" {
-	 		return fmt.Errorf("cluster name is missing")
+		return fmt.Errorf("cluster name is missing")
 	}
 
 	if o.machinePoolName == "" {
 		return fmt.Errorf("machinepool is missing")
-  }
+	}
 
 	// // replicas defaults
 	if o.replicas == 0 {
 		return fmt.Errorf("replicas is missing")
-  }
+	}
 
 	return nil
 }
@@ -78,107 +78,3 @@ func (o *Options) runWithClient(client crclient.Client) error {
 	spec["replicas"] = o.replicas
 	return client.Patch(context.TODO(), mp, patch)
 }
-
-/*
-func (o *Options) runWithClient(client crclient.Client) error {
-
-	reader := scenario.GetApplierScenarioResourcesReader()
-	mp := unstructured.Unstructured{}
-	//mp.SetKind("MachinePool")
-	//mp.SetAPIVersion("hive.openshift.io/v1")
-
-	mp.SetGroupVersionKind(schema.GroupVersionKind{
-		Group:   "hive.openshift.io",
-		Kind:    "MachinePool",
-		Version: "v1",
-	})
-
-	klog.Info("get MachinePool entry")
-	err := client.Get(context.TODO(),
-		crclient.ObjectKey{
-			Name:      o.machinePoolName,
-			Namespace: o.clusterName,
-		}, &mp)
-	klog.Info("get MachinePool entry - return.  %v", mp)
-
-	if err == nil {
-
-		//	gvrMachinePool = schema.GroupVersionResource{Group: "hive.openshift.io", Version: "v1", Resource: "machinepool"}
-		//	clientHubDynamic, err = libgoclient.NewDefaultKubeClientDynamic("")
-		//	libgoclient.
-
-		// add place holder
-		//patchString := `{"spec": {"replicas": 3 }}`
-		patchInBytes := []byte(fmt.Sprintf(`{"spec":{replicas":"%d"}}`, o.replicas))
-
-		//		_, err := clientHubDynamic.Resource(gvrMachinePool).Namespace("").Patch(context.TODO(), name, types.MergePatchType, []byte(patchString), metav1.PatchOptions{}, "status")
-		//_, err := crclient.Patch(context.TODO(), mp, types.MergePatchType, []byte(patchString), metav1.PatchOptions{})
-		//_, err := crclient.Patch(context.TODO(), mp, crclient.RawPatch(types.MergePatchType, patch))
-		//_, err := crclient.Patch(context.TODO(), mp, types.JSONPatchType, patchInBytes, v1.PatchOptions{})
-		//_, err := crclient.Patch(context.TODO(), mp, crclient.RawPatch(types.MergePatchType, patchInBytes))
-
-		klog.V(1).Info("Change replicas to %d", o.replicas)
-
-		//crclient.RawPatch(types.StrategicMergePatchType, patchInBytes)
-		//crclient.RawPatch(types.MergePatchType, patchInBytes)
-		//crclient.RawPatch(types.ApplyPatchType, patchInBytes)
-		crclient.RawPatch(types.JSONPatchType, patchInBytes)
-
-		//  patchStringValue specifies a json patch operation for a string.
-		//	type patchStringValue struct {
-		//		Op    string `json:"op"`
-		//		Path  string `json:"path"`
-		//		Value string `json:"value"`
-		//	}
-
-		//	patch := []patchStringValue{{
-		//		Op:    "replace",
-		//		Path:  "/spec/replicas",
-		//		Value: strconv.Itoa(o.replicas),
-		//	}}
-		//patchInBytes, _ := json.Marshal(patch)
-		//klog.V(2).Info(" > Patching secret " + secretName + " in namespace " + clusterName)
-		//_, err = crclient.Patch(context.TODO(), mp, types.JSONPatchType, patchInBytes, v1.PatchOptions{})
-
-		//_, err = crclient.Patch(context.TODO(), mp, types.JSONPatchType, patchInBytes, metav1.PatchOptions{})
-
-		if err != nil {
-			panic(fmt.Errorf("Failed to scale replicas: %v", err))
-		}
-
-		//Another try
-
-		//		//spec.replicas
-		//		retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		//	// Retrieve the latest version of Deployment before attempting update
-		//	// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-		//	result, getErr := client.Resource(deploymentRes).Namespace(namespace).Get("demo-deployment", metav1.GetOptions{})
-		//	if getErr != nil {
-		//		panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
-		//	}
-		//
-		//	// update replicas to 1
-		//	if err := unstructured.SetNestedField(result.Object, int64(1), "spec", "replicas"); err != nil {
-		//		panic(fmt.Errorf("Failed to set replica value: %v", err))
-		//	}
-
-	} else {
-		return fmt.Errorf("MachinePool not found! %v", err)
-	}
-
-	applyOptions := &appliercmd.Options{
-		OutFile:     o.applierScenariosOptions.OutFile,
-		ConfigFlags: o.applierScenariosOptions.ConfigFlags,
-
-		Delete:    false,
-		Timeout:   o.applierScenariosOptions.Timeout,
-		Force:     o.applierScenariosOptions.Force,
-		Silent:    o.applierScenariosOptions.Silent,
-		IOStreams: o.applierScenariosOptions.IOStreams,
-	}
-
-	return applyOptions.ApplyWithValues(client, reader,
-		filepath.Join(scenarioDirectory, "hub", "common"),
-		o.values)
-}
-*/
