42fd9ec64f5eb485bacb8d9562d8bd0a75c945b2
Dominique Vernier <dvernier@redhat.com>
2022-02-11 14:59:33 -0500

    Alias detach clusterclaim with cluster
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- pkg/cmd/detach/cluster/cmd.go
+++ pkg/cmd/detach/cluster/cmd.go
@@ -39,7 +39,7 @@ func NewCmd(cmFlags *genericclioptionscm.CMFlags, streams genericclioptions.IOSt
 
 	cluster := &cobra.Command{
 		Use:          "cluster",
-		Aliases:      []string{"clusters", "clusterclaim", "clusterclaims", "cc", "ccs", "hostedcluster", "hostedclusters", "hc", "hcs"},
+		Aliases:      []string{"clusters", "cluster", "clusterclaim", "clusterclaims", "cc", "ccs", "hostedcluster", "hostedclusters", "hc", "hcs"},
 		Short:        "detach a cluster, clusterclaim or hostedcluster",
 		Example:      fmt.Sprintf(example, helpers.GetExampleHeader()),
 		SilenceUsage: true,
